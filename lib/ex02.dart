f684bde54a327bb2bf6930630c4e6e7011713bcc
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-16 19:46:16 -0300

    feat: :sparkles: 16/09 - activity until today

---
+++ lib/ex02.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        'Andre Isaac',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ), //Text.
+    ), //Center.
+  ); //runApp.
+}
